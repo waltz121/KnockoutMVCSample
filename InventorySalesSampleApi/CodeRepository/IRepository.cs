d550bc01bc49bda4dfc9138ac999a3a3016ad6c7
Walter R. Robledo <robleo.walterrobbin@gmail.com>
2019-10-29 11:59:32 +0800

    Added Product Repository and Initial Repository Pattern

---
+++ InventorySalesSampleApi/CodeRepository/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventorySalesSampleApi.CodeRepository
+{
+    public interface IRepository<T>
+    {
+        IEnumerable<T> GetAll();
+        T Get();
+        string Update(T UpdatedEntity);
+        string Add(T Newentity);
+        string Delete();
+        IQueryable<T> Find(Expression<Func<T, bool>> predicate);
+    }
+}
---
+++ InventorySalesSampleApi/CodeRepository/ProductRepository.cs
@@ -0,0 +1,50 @@
+﻿using InventorySalesSampleApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace InventorySalesSampleApi.CodeRepository
+{
+    public class ProductRepository : IRepository<Product>
+    {
+        InventorySalesDBEntities DBEntities;
+        public ProductRepository(InventorySalesDBEntities inventorySalesDBEntities)
+        {
+            DBEntities = inventorySalesDBEntities;
+        }
+        public string Add(Product Newentity)
+        {
+            DBEntities.Products.Add(Newentity);
+            return "Newly Added State";
+        }
+
+        public string Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Product> Find(Expression<Func<Product, bool>> predicate)
+        {
+            return DBEntities.Products.Where(predicate);
+        }
+
+        public Product Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return DBEntities.Products.OrderBy(x => x.Id).ToList();
+        }
+
+        public string Update(Product UpdatedEntity)
+        {
+            DBEntities.Entry(UpdatedEntity).State = EntityState.Modified;
+            return "Modified State";
+        }
+    }
+}
